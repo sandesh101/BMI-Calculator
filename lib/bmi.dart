34b481be1e73974bc308da4347b8a36671b476b0
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-27 21:58:54 +0545

    Added Height Input Field

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -9,6 +9,7 @@ class _BmiUIState extends State<BmiUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF181823),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text('BMI Calculator'),
@@ -17,7 +18,27 @@ class _BmiUIState extends State<BmiUI> {
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
             child: Icon(Icons.more_vert),
-          )
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Container(
+              width: 130,
+              child: TextField(
+                style: TextStyle(color: Colors.white70, fontSize: 30),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Height',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  hintStyle: TextStyle(color: Colors.white70),
+                ),
+              ),
+            ),
+          ]),
         ],
       ),
     );
